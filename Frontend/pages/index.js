07380575cff69175468d2daddcb666d8ee3b7659
张任宇 <95209873+zhangreny@users.noreply.github.com>
2023-08-28 18:05:49 +0800

    add default content of KGDB and popuplayer of adding KGDB connection

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,3 +1,27 @@
 function ChangePage(pagename) {
     console.log(pagename);
 }
+
+function OpenPopup(divid) {
+    document.getElementById('popup-content').classList.remove("hidden")
+    document.getElementById(divid).classList.remove("hidden")
+    document.getElementById('popup').classList.remove("hidden")
+    document.getElementById('popup-content').classList.add("layui-anim-up")
+}
+
+function ClosePopup() {
+    // 隐藏所有的子界面
+    var parentElement = document.getElementById('popup');
+    var idDivs = parentElement.querySelectorAll('div[id]');
+    idDivs.forEach(function(div) {
+        div.classList.add("hidden")
+    });
+    // 隐藏弹出层
+    document.getElementById('popup-content').classList.remove("layui-anim-up")
+    parentElement.classList.add("hidden")
+}
+
+function AddNewDBConnection() {
+    var elements = document.getElementsByTagName("input")
+
+}
